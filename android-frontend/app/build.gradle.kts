729d70adb242eef837d86f0d3d90e0b48b81173a
Ryan Montambeault <montambeault566@gmail.com>
2025-03-25 23:21:22 -0300

    Adding Dependencies

--- android-frontend/app/build.gradle.kts
+++ android-frontend/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("kapt") version "2.0.0"
 }
 
 android {
@@ -40,8 +41,24 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Gson for parsing
+    implementation(libs.google.gson)
+
+    // Retrofit for making HTTP requests
+    implementation(libs.retrofit)
+    implementation(libs.converter)
+
+    // OkHTTP for building HTTP Request objects
+    implementation(libs.parent)
+    implementation(libs.logging.interceptor)
+
+    // Kotlin coroutines for asynchronous operations off the main thread
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.androidx.activity)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ android-frontend/app/src/main/java/com/example/schedulingapp_android/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.schedulingapp_android.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.schedulingapp_android.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
